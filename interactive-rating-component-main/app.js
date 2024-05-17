8ae5920aa83cb1a6532908ac49b7a292566bcea2
manav-sharma69 <157268750+manav-sharma69@users.noreply.github.com>
2024-05-17 22:46:00 +0530

    Update app.js

--- interactive-rating-component-main/app.js
+++ interactive-rating-component-main/app.js
@@ -1,15 +1,7 @@
-if(localStorage.getItem('ratingAdded') == null) localStorage.setItem('ratingAdded', 'false');
-else{
-    document.querySelector('.rating').textContent = localStorage.getItem('ratingAdded');
-    document.querySelector('dialog').show();
-    document.querySelector('form').style.display = 'none';  
-}
 
 document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
     document.querySelector('dialog').show();
     document.querySelector('form').style.display = 'none'; 
     document.querySelector('.rating').textContent = document.querySelector('input:checked').value;
-    localStorage.setItem('ratingAdded', `${document.querySelector('input:checked').value}`);
-    console.log(localStorage.getItem('ratingAdded'));
 })
